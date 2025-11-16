80194d4f052d95bbba84856ec03dbe5f89b31094
MennaElRouby <roubymenna@gmail.com>
2025-11-16 11:49:17 +0200

    Develop Contact page

--- resources/views/Website/home-partials/contact.blade.php
+++ resources/views/Website/home-partials/contact.blade.php
@@ -1,5 +1,5 @@
 <!-- <================================================================= StartContactForm =======================================================> -->
-        <section class="subscribe-section">
+{{-- <section class="subscribe-section">
             <div class="bg bg-image" style="background-image: url(uploads/titles/source/68715da79a5f6.png);"></div>
             <div class="auto-container">
                 <div class="outer-box">
@@ -23,5 +23,71 @@
                     </div>
                 </div>
             </div>
-        </section>
+        </section> --}}
+<section class="contact-details">
+    <div class="container ">
+        <div class="row">
+            <div class="col-xl-7 col-lg-6">
+                <div class="sec-title">
+                    <span class="sub-title">Send us email</span>
+                    <h2>Feel free to write</h2>
+                </div>
+                <!-- Contact Form -->
+               @include('components.website.partials.form')
+            </div>
+            <div class="col-xl-5 col-lg-6">
+                <div class="contact-details__right">
+                    <div class="sec-title">
+                        <h2>Get in touch</h2>
+                    </div>
+                    <ul class="list-unstyled contact-details__info">
+                        <li>
+                            <div class="icon">
+                                <span class="fas fa-phone-alt"></span>
+                            </div>
+                            <div class="text">
+                                <h6>Call Us</h6>
+                                @foreach ($phones as $phone)
+                                    <a href="tel:+{{ $phone->code }}{{ $phone->phone }}">{{ $phone->phone }}</a>
+                                    <br>
+                                @endforeach
+                            </div>
+                        </li>
+                        <li>
+                            <div class="icon">
+                                <span class="fas fa-envelope"></span>
+                            </div>
+                            <div class="text">
+                                <h6>E.mail</h6>
+                                <a href="mailto:{{ config('settings.site_email') }}">{{ config('settings.site_email') }}</a>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="icon">
+                                <span class="fas fa-map-marker"></span>
+                            </div>
+                            <div class="text">
+                                <h6>Addresses</h6>
+                                @foreach ($site_addresses as $address)
+                                    <span>
+                                        <a href="{{ $address->map_link }}" target="_blank">{{ $address->address }}</a>
+                                    </span>
+                                    <br>
+                                @endforeach
+                            </div>
+                        </li>
+
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--Contact Details End-->
+
+<!-- Map Section-->
+<section class="map-section">
+    @include('Website.home-partials.map')
+</section>
+<!--End Map Section-->
 <!-- <======================= EndContactForm =========================> -->
---
+++ resources/views/components/website/partials/form.blade.php
@@ -0,0 +1,71 @@
+ @if ($errors->any())
+     <div class="alert alert-danger">
+         <ul>
+             @foreach ($errors->all() as $error)
+                 <li>{{ $error }}</li>
+             @endforeach
+         </ul>
+     </div>
+ @endif
+ <form id="contact_form" name="contact_form" class="" action="{{ route('website.saveConatct') }}" method="post">
+     @csrf
+     <input type="hidden" name="_token" value="FtMWS0ink9X7SIeCmhFWc8JMLYzv92lavdMKxsjF" autocomplete="off">
+     <div class="row">
+         <input type="hidden" name="recaptcha_token" id="recaptcha_token">
+         <div class="col-sm-6">
+             <div class="mb-3">
+                 <input name="name" class="form-control" type="text" value="{{ old('name') }}"
+                     placeholder="{{ __('website.name') }}" required>
+             </div>
+         </div>
+         <div class="col-sm-6">
+             <div class="mb-3">
+                 <input name="email" class="form-control" type="email" value="{{ old('email') }}"
+                     placeholder="{{ __('website.email') }}" required>
+             </div>
+         </div>
+     </div>
+     <div class="row">
+
+
+         <div class="col-sm-12">
+             <div class="mb-3">
+                 <input name="phone" class="form-control" type="text" value="{{ old('phone') }}"
+                     placeholder="{{ __('website.phone') }}" required>
+             </div>
+         </div>
+     </div>
+     <div class="mb-3">
+         <textarea name="message" class="form-control required" rows="7" placeholder="{{ __('website.message') }}">{{ old('message') }}</textarea>
+     </div>
+     @if (config('captcha.sitekey') && config('captcha.sitekey') !== 'Key')
+         <div class="form-group col-lg-12 col-md-12">
+             <div class="g-recaptcha" data-sitekey="{{ config('captcha.sitekey') }}"></div>
+         </div>
+     @endif
+     <div class="mb-5">
+         <input name="form_botcheck" class="form-control" type="hidden" value="" />
+         <button type="submit" class="theme-btn btn-style-one" data-loading-text="Please wait..."><span
+                 class="btn-title">{{ __('website.send') }}</span></button>
+     </div>
+ </form>
+ @if (config('captcha.sitekey') && config('captcha.sitekey') !== 'Key')
+     <script src="https://www.google.com/recaptcha/api.js?render={{ config('captcha.sitekey') }}"></script>
+
+     <script>
+         grecaptcha.ready(function() {
+             grecaptcha.execute('{{ config('captcha.sitekey') }}', {
+                 action: 'contact'
+             }).then(function(token) {
+                 document.getElementById('recaptcha_token').value = token;
+             }).catch(function(error) {
+                 console.warn('reCAPTCHA error:', error);
+             });
+         });
+     </script>
+ @else
+     <script>
+         console.warn('reCAPTCHA is not configured properly. Please set NOCAPTCHA_SITEKEY in your .env file');
+     </script>
+ @endif
+ <!-- Contact Form Validation-->
